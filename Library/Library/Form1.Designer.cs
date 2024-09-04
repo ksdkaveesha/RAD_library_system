8c5b6d32b57315391d1a270c90a6aa1054bc271f
ksdkaveesha <ksdkaveesha@gmail.com>
2024-09-04 19:07:25 +0530

    new project

---
+++ Library/Library/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Library
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackgroundImage = Properties.Resources._3d_cartoon_scene_depicting_variety_people_multitasking;
+            panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            panel1.Location = new Point(-7, -39);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(411, 623);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackgroundImage = Properties.Resources.books_with_graduation_cap_digital_art_style_education_day;
+            panel2.BackgroundImageLayout = ImageLayout.Stretch;
+            panel2.Location = new Point(458, 36);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(65, 72);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Script MT Bold", 30F, FontStyle.Bold);
+            label1.Location = new Point(526, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(223, 48);
+            label1.TabIndex = 2;
+            label1.Text = "Book Haven";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label label1;
+    }
+}
--- Library_System/Library_System/books.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Title] VARCHAR(50) NOT NULL, 
-    [ISBN] INT NULL, 
-    [Genre] VARCHAR(50) NOT NULL, 
-    [AvailableCopies] INT NOT NULL
-)
