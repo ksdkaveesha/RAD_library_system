72fd8245ea5e9e1df0f3ecce80f32b9f6bdb886b
ksdkaveesha <ksdkaveesha@gmail.com>
2024-09-05 00:09:27 +0530

    view,return,add,issue book pages created

--- Library/Library/obj/Debug/net8.0-windows/Library.AssemblyInfo.cs
+++ Library/Library/obj/Debug/net8.0-windows/Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56fe6c174d43ced9a812e2158dd3a818fd6105a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5292248938f8696469e18075d82679249e8d5cc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
