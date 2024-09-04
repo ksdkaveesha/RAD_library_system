56fe6c174d43ced9a812e2158dd3a818fd6105a6
ksdkaveesha <ksdkaveesha@gmail.com>
2024-09-04 19:30:16 +0530

    login

--- Library/Library/Form1.Designer.cs
+++ Library/Library/Form1.Designer.cs
@@ -31,6 +31,15 @@
             panel1 = new Panel();
             panel2 = new Panel();
             label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            button1 = new Button();
+            label4 = new Label();
+            linkLabel1 = new LinkLabel();
             SuspendLayout();
             // 
             // panel1
@@ -61,12 +70,113 @@
             label1.TabIndex = 2;
             label1.Text = "Book Haven";
             // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Control;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Cursor = Cursors.IBeam;
+            textBox1.Font = new Font("Segoe UI", 12F);
+            textBox1.ForeColor = SystemColors.ControlDark;
+            textBox1.Location = new Point(461, 183);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(275, 22);
+            textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic);
+            label2.ForeColor = SystemColors.ActiveCaptionText;
+            label2.Location = new Point(457, 159);
+            label2.Name = "label2";
+            label2.Size = new Size(92, 21);
+            label2.TabIndex = 4;
+            label2.Text = "User Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic);
+            label3.ForeColor = SystemColors.ActiveCaptionText;
+            label3.Location = new Point(457, 231);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 21);
+            label3.TabIndex = 6;
+            label3.Text = "Password";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Control;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Cursor = Cursors.IBeam;
+            textBox2.Font = new Font("Segoe UI", 12F);
+            textBox2.ForeColor = SystemColors.ControlDark;
+            textBox2.Location = new Point(461, 255);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(275, 22);
+            textBox2.TabIndex = 5;
+            textBox2.UseSystemPasswordChar = true;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ActiveCaptionText;
+            panel3.Location = new Point(462, 209);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(275, 1);
+            panel3.TabIndex = 7;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = SystemColors.ActiveCaptionText;
+            panel4.Location = new Point(461, 281);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(275, 1);
+            panel4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 13F, FontStyle.Bold);
+            button1.Location = new Point(513, 306);
+            button1.Name = "button1";
+            button1.Size = new Size(181, 40);
+            button1.TabIndex = 9;
+            button1.Text = "Log In";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(475, 389);
+            label4.Name = "label4";
+            label4.Size = new Size(163, 15);
+            label4.TabIndex = 10;
+            label4.Text = "If you don't have an account?";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(635, 389);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(91, 15);
+            linkLabel1.TabIndex = 11;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Sign Up for Free";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
             ClientSize = new Size(800, 450);
+            Controls.Add(linkLabel1);
+            Controls.Add(label4);
+            Controls.Add(button1);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
             Controls.Add(label1);
             Controls.Add(panel2);
             Controls.Add(panel1);
@@ -74,7 +184,7 @@
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "Form1";
-            Text = "Form1";
+            Text = "Book Haven Library";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -84,5 +194,14 @@
         private Panel panel1;
         private Panel panel2;
         private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Panel panel3;
+        private Panel panel4;
+        private Button button1;
+        private Label label4;
+        private LinkLabel linkLabel1;
     }
 }
